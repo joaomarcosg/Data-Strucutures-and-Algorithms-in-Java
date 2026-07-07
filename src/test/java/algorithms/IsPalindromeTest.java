58a28d294f0259c83e7fef514e08792282129393
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-07 15:08:08 -0300

    test: adds initial test

---
+++ src/test/java/algorithms/IsPalindromeTest.java
@@ -0,0 +1,20 @@
+package test.java.algorithms;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IsPalindromeTest {
+
+    @Test
+    @DisplayName("Should return true if the word or phrase is a palindrome")
+    void checkPalindrome() {
+        String text = "Anotaram a data da maratona";
+
+        boolean result = IsPalindrome.checkString(text);
+
+        assertTrue(result, "The string is a palindrome");
+    }
+}
