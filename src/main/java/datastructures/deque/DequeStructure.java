90ed3efccb5ecdf58b5956dd37f34a9f5fab712c
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-09 16:32:25 -0300

    feat: adds a dynamic deque

---
+++ src/main/java/datastructures/deque/DequeStructure.java
@@ -0,0 +1,39 @@
+package main.java.datastructures.deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Objects;
+
+public class DequeStructure<T> {
+    private Deque<T> deque = new ArrayDeque<>();
+
+    public DequeStructure() {}
+
+    public boolean addFront(T element) {
+        Objects.requireNonNull(element, "The element cannot be null");
+        this.deque.addFirst(element);
+        return true;
+    }
+
+    public boolean addRear(T element) {
+        Objects.requireNonNull(element, "The element cannot be null");
+        this.deque.addLast(element);
+        return true;
+    }
+
+    public T removeFront() {
+        return this.deque.pollFirst();
+    }
+
+    public T removeRear() {
+        return this.deque.pollLast();
+    }
+
+    public boolean isEmpty() {
+        return this.deque.isEmpty();
+    }
+
+    public int size() {
+        return this.deque.size();
+    }
+}
