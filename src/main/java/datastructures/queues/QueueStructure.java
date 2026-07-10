93a7ccf907b7a9a2f0bb1dd4981f2187f6ae0080
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-10 16:09:35 -0300

    feat: implements a dynamic queue

---
+++ src/main/java/datastructures/queues/QueueStructure.java
@@ -0,0 +1,26 @@
+package main.java.datastructures.queues;
+
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+
+public class QueueStructure<T> {
+    private final Queue<T> queue = new LinkedList<>();
+
+    public QueueStructure() {}
+
+    public boolean enqueue(T element) {
+        Objects.requireNonNull(element, "The element cannot be null.");
+        return this.queue.offer(element);
+    }
+
+    public T dequeue() {
+        if (this.queue.isEmpty()) return null;
+        return this.queue.poll();
+    }
+
+    public T peek() {
+        if (this.queue.isEmpty()) return null;
+        return this.queue.peek();
+    }
+}
