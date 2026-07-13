42d567b230cad11b4f1d6c85137229f7158a2b5b
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-13 17:13:14 -0300

    feat: adds linked list methods

---
+++ src/main/java/datastructures/linkedlist/LinkedListStructure.java
@@ -0,0 +1,45 @@
+package main.java.datastructures.linkedlist;
+
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class LinkedListStructure<T> {
+
+    private final LinkedList<T> list = new LinkedList<>();
+
+    public LinkedListStructure() {}
+
+    public boolean push(T element) {
+        Objects.requireNonNull(element, "The element cannot be null");
+        this.list.addLast(element);
+        return true;
+    }
+
+    public T removeAt(int index) {
+        if (index < 0 || index >= this.list.size()) {
+            throw  new IndexOutOfBoundsException();
+        }
+        return this.list.remove(index);
+    }
+
+    public void insert(int index, T element) {
+        Objects.requireNonNull(element, "The element cannot be null");
+        if (index < 0 || index > this.list.size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        this.list.add(index, element);
+    }
+
+    public T peek() {
+        return this.list.peekFirst();
+    }
+
+    public int size() {
+        return this.list.size();
+    }
+
+    public boolean isEmpty() {
+        return this.list.isEmpty();
+    }
+
+}
