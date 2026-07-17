3b35a2b40938e11bd2d165f10db733e7cf062724
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-17 18:18:50 -0300

    test: adds initial tests to the sets

---
+++ src/test/java/datastructures/sets/SetStructureTest.java
@@ -0,0 +1,43 @@
+package test.java.datastructures.sets;
+
+import main.java.datastructures.sets.SetStructure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class SetStructureTest {
+
+    private SetStructure<String> setStructure;
+
+    @BeforeEach
+    void setUp() {
+        setStructure = new SetStructure<>();
+    }
+
+    @Test
+    void should_add_element_successfully() {
+        boolean result = setStructure.add("John");
+
+        assertTrue(result);
+        assertTrue(setStructure.has("John"));
+    }
+
+    @Test
+    void should_not_add_duplicate_element() {
+        setStructure.add("John");
+        boolean result = setStructure.add("John");
+
+        assertFalse(result);
+        assertEquals(1, setStructure.size());
+    }
+
+    @Test
+    void should_return_true_when_element_exists() {
+        setStructure.add("John");
+
+        assertTrue(setStructure.has("John"));
+    }
+
+
+}
