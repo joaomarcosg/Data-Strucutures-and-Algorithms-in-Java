ca7e2e2098869a4eacd522cc33ac51acadd2717f
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-18 13:27:46 -0300

    feat: adds operations to the set

--- src/main/java/datastructures/sets/SetStructure.java
+++ src/main/java/datastructures/sets/SetStructure.java
@@ -34,5 +34,52 @@ public class SetStructure<T> {
     public Set<T> values() {
         return new HashSet<>(this.set);
     }
-    
+
+    public SetStructure<T> union(SetStructure<T> otherSet) {
+        SetStructure<T> unionSet = new SetStructure<>();
+
+        unionSet.set.addAll(this.set);
+        unionSet.set.addAll(otherSet.set);
+
+        return unionSet;
+    }
+
+    public SetStructure<T> intersection(SetStructure<T> otherSet) {
+        SetStructure<T> intersectionSet = new SetStructure<>();
+
+        Set<T> biggerSet = this.values();
+        Set<T> smallerSet = otherSet.values();
+
+        if (otherSet.size() > this.size()) {
+            biggerSet = otherSet.values();
+            smallerSet = this.values();
+        }
+
+        Set<T> finalBiggerSet = biggerSet;
+        smallerSet.forEach(e -> {
+            if (finalBiggerSet.contains(e)) {
+                intersectionSet.add(e);
+            }
+        });
+
+        return intersectionSet;
+    }
+
+    public SetStructure<T> difference(SetStructure<T> otherSet) {
+        SetStructure<T> differenceSet = new SetStructure<>();
+
+        this.set.forEach(e -> {
+            if (!otherSet.has(e)) {
+                differenceSet.add(e);
+            }
+        });
+
+        return differenceSet;
+    }
+
+    public boolean isSubsetOf(SetStructure<T> otherSet) {
+        if (this.size() > otherSet.size()) return false;
+
+        return this.set.stream().allMatch(otherSet::has);
+    }
 }
