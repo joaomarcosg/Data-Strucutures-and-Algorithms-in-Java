1b1f6320f01ae6d0979906d24f10dfc74e1671c6
joaomarcosg <joaomarcos.ipms@gmail.com>
2026-07-18 12:47:01 -0300

    test: adds difference operation tests

--- src/test/java/datastructures/sets/SetStructureTest.java
+++ src/test/java/datastructures/sets/SetStructureTest.java
@@ -102,5 +102,60 @@ public class SetStructureTest {
 
         assertTrue(result.isEmpty());
     }
-    
+
+    @Test
+    void should_return_only_elements_in_the_first_set() {
+        SetStructure<String> setA = new SetStructure<>();
+        setA.add("John");
+        setA.add("Jack");
+        setA.add("Camila");
+
+        SetStructure<String> setB = new SetStructure<>();
+        setB.add("John");
+        setB.add("Tom");
+
+        SetStructure<String> result = setA.difference(setB);
+
+        assertEquals(2, result.size());
+        assertTrue(result.has("Jack"));
+        assertTrue(result.has("Camila"));
+        assertFalse(result.has("John"));
+    }
+
+    @Test
+    void should_return_empty_set_when_sets_are_equals() {
+        SetStructure<String> setA = new SetStructure<>();
+        setA.add("John");
+        setA.add("Jack");
+
+        SetStructure<String> setB = new SetStructure<>();
+        setB.add("John");
+        setB.add("Jack");
+
+        SetStructure<String> result = setA.difference(setB);
+
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void difference_is_not_commutative() {
+        SetStructure<String> setA = new SetStructure<>();
+        setA.add("John");
+        setA.add("Jack");
+
+        SetStructure<String> setB = new SetStructure<>();
+        setB.add("John");
+        setB.add("Camila");
+
+        SetStructure<String> resultAB = setA.difference(setB);
+        SetStructure<String> resultBA = setB.difference(setA);
+
+        assertEquals(1, resultAB.size());
+        assertTrue(resultAB.has("Jack"));
+
+        assertEquals(1, resultBA.size());
+        assertTrue(resultBA.has("Camila"));
+        
+    }
+
 }
