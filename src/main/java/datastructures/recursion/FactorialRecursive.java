de2f0af35c40e841fc781ee0001a7d61df30c0b7
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-08-29 17:44:10 -0300

    refactor: refactor recursive method adding an exception and changing number verification

--- src/main/java/datastructures/recursion/FactorialRecursive.java
+++ src/main/java/datastructures/recursion/FactorialRecursive.java
@@ -3,7 +3,10 @@ package main.java.datastructures.recursion;
 public class FactorialRecursive {
 
     public static long factorial(int number) {
-        if (number == 0 || number == 1) {
+        if (number < 0) {
+            throw new IllegalArgumentException("The number must be greater than or equal to zero ");
+        }
+        if (number <= 1) {
             return 1;
         }
         return number * factorial(number - 1);
