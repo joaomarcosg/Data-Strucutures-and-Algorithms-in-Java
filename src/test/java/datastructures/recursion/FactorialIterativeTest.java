6edadf430ef6c0af737de6d48b2225dab8d4e14d
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-08-29 17:30:55 -0300

    test: adds initial test

--- src/test/java/datastructures/recursion/FactorialIterativeTest.java
+++ src/test/java/datastructures/recursion/FactorialIterativeTest.java
@@ -15,5 +15,14 @@ public class FactorialIterativeTest {
         assertEquals(1, result);
     }
 
+    @Test
+    @DisplayName("Should calculate factorial of small positive numbers")
+    void shouldCalculateFactorialForSmallNumbers() {
+        assertEquals(1, FactorialIterative.factorial(1));
+        assertEquals(2, FactorialIterative.factorial(2));
+        assertEquals(6, FactorialIterative.factorial(3));
+        assertEquals(24, FactorialIterative.factorial(4));
+        assertEquals(120, FactorialIterative.factorial(5));
+    }
 
 }
