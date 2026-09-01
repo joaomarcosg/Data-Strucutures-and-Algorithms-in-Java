9aa954c3536b8497585317b5685b1bd4036539b5
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-01 16:56:02 -0300

    fix: fixes loop again

--- src/main/java/datastructures/recursion/FibonacciIterative.java
+++ src/main/java/datastructures/recursion/FibonacciIterative.java
@@ -8,7 +8,7 @@ public class FibonacciIterative {
 
         int a = 0, b = 1;
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             int next = a + b;
             a = b;
             b = next;
