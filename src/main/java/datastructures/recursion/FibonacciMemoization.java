292dc3921c573222ffb26f04e77616450dff48e0
João Marcos Santana Gomes <joaomarcos.ipms@gmail.com>
2026-09-11 16:23:13 -0300

    feat: adds method to calculate nth term of Fibonacci sequence using memoization

---
+++ src/main/java/datastructures/recursion/FibonacciMemoization.java
@@ -0,0 +1,28 @@
+package main.java.datastructures.recursion;
+
+import java.util.Arrays;
+
+public class FibonacciMemoization {
+
+    public static long getNthFibonacci(final int n) {
+        if (n < 1) throw new IllegalArgumentException("Index cannot be negative");
+
+        long[] memo = new long[n + 1];
+
+        Arrays.fill(memo, -1);
+
+        return getNthFibonacci(n, memo);
+    }
+
+    private static long getNthFibonacci(final int n, final long[] memo) {
+        if (n <= 1) return n;
+
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+
+        memo[n] = getNthFibonacci(n - 1, memo) + getNthFibonacci(n - 2, memo);
+
+        return memo[n];
+    }
+}
